8a5e5a86063f1cabfe529a43129e6e7c7fb77c2f
egarduno <edgarantonio.garduno@icloud.com>
2025-06-29 16:01:23 -0700

    Fixed imgs paths to display them ropertly on hosting web

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sandylandlandscape-landing/',
+  base: './',
   plugins: [react(),
     tailwindcss(),
   ],
